7fbf30ac28d50788a467f0fd4c50abfa49d6f373
KyungjinJPark <kjparkcontact@gmail.com>
2020-10-16 03:10:53 -0400

    shrinking design to start animations easy

---
+++ main-and-week src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import "./App.css";
+import Content from "./Content.js";
+
+const App = () => {
+  return (
+    <div id="main">
+      <Content />
+    </div>
+  );
+};
+
+export default App;
